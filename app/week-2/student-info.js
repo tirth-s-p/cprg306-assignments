efd990b0196c8b9bda917c05ff84a41f6c741c13
tirth-s-patel <tirthsanjivkumar.patel@edu.sait.ca>
2024-09-24 21:36:22 -0600

    main

---
+++ app/week-2/student-info.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const StudentInfo = () => {
+    return (
+        <div>
+            <h1>Tirth Patel</h1>
+            <p>
+                <a href="https://github.com/tirth-s-p" target="_blank">
+                https://github.com
+                </a>
+            </p>
+        </div>
+    );
+};
+
+export default StudentInfo;
