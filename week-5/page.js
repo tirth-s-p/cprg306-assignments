14650dfb22530b8a51cc89b1d547ffec7463aeb9
Tirth-Patel <152663004+tirth-s-p@users.noreply.github.com>
2024-10-15 13:48:40 -0600

    Add files via upload

---
+++ week-5/page.js
@@ -0,0 +1,11 @@
+import NewItem from "./new-item";
+
+export default function Week5Page() {
+  return (
+    <div className="min-h-screen bg-black flex items-start justify-center">
+      <div className="w-full max-w-lg p-8">
+        <NewItem />
+      </div>
+    </div>
+  );
+}
