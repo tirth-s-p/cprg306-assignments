a00fac639419ab787ca3e3f71f9dcacc9c73faff
Tirth-Patel <152663004+tirth-s-p@users.noreply.github.com>
2024-11-18 16:47:50 -0700

    Create page.js

---
+++ app/week-10/page.js
@@ -0,0 +1,27 @@
+"use client";
+
+import Link from "next/link";
+import { useUserAuth } from "./_utils/auth-context";
+
+export default function LandingPage() {
+  const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
+
+  return (
+    <div>
+
+      {!user ? (
+        <div>
+          <p>Please sign in to continue.</p>
+          <button onClick={gitHubSignIn}>Sign in with GitHub</button>
+        </div>
+      ) : (
+        <div>
+          <p>Signed in as {user.displayName} ({user.email}).</p>
+          <button onClick={firebaseSignOut}>Sign out</button>
+          <br />
+          <Link href="/week-10/shopping-list">Continue to your Shopping List</Link>
+        </div>
+      )}
+    </div>
+  );
+}
